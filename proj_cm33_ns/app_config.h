3875fae0668b7776353e9118262dfeeb08b68247
Nikola Markovic <nikola.m@gmail.com>
2025-10-10 10:10:36 -0500

    app_config.h cleanup

--- proj_cm33_ns/app_config.h
+++ proj_cm33_ns/app_config.h
@@ -1,44 +1,7 @@
-/******************************************************************************
-* File Name:   mqtt_client_config.h
-*
-* Description: This file contains all the configuration macros used by the
-*              MQTT client in this example.
-*
-* Related Document: See README.md
-*
-*
-*******************************************************************************
-* Copyright 2024-2025, Cypress Semiconductor Corporation (an Infineon company) or
-* an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
-*
-* This software, including source code, documentation and related
-* materials ("Software") is owned by Cypress Semiconductor Corporation
-* or one of its affiliates ("Cypress") and is protected by and subject to
-* worldwide patent protection (United States and foreign),
-* United States copyright laws and international treaty provisions.
-* Therefore, you may use this Software only as provided in the license
-* agreement accompanying the software package from which you
-* obtained this Software ("EULA").
-* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
-* non-transferable license to copy, modify, and compile the Software
-* source code solely for use in connection with Cypress's
-* integrated circuit products.  Any reproduction, modification, translation,
-* compilation, or representation of this Software except as specified
-* above is prohibited without the express written permission of Cypress.
-*
-* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
-* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
-* reserves the right to make changes to the Software without notice. Cypress
-* does not assume any liability arising out of the application or use of the
-* Software or any product or circuit described in the Software. Cypress does
-* not authorize its products for use in any products where a malfunction or
-* failure of the Cypress product may reasonably be expected to result in
-* significant property damage, injury or death ("High Risk Product"). By
-* including Cypress's product in a High Risk Product, the manufacturer
-* of such system or application assumes all risk of such use and in doing
-* so agrees to indemnify Cypress against all liability.
-*******************************************************************************/
+/* SPDX-License-Identifier: MIT
+ * Copyright (C) 2025 Avnet
+ * Authors: Nikola Markovic <nikola.markovic@avnet.com>, Shu Liu <shu.liu@avnet.com> et al.
+ */
 
 #ifndef APP_CONFIG_H
 #define APP_CONFIG_H
@@ -46,12 +9,14 @@
 #include "iotconnect.h"
 
 // see IotConnectConnectionType: IOTC_CT_AWS or IOTC_CT_AZURE
-#define IOTCONNECT_CONNECTION_TYPE IOTC_CT_AWS
-#define IOTCONNECT_CPID "MYCPID"
-#define IOTCONNECT_ENV  "poc"
+#define IOTCONNECT_CONNECTION_TYPE IOTC_CT_UNDEFINED
+#define IOTCONNECT_CPID "your-cpid"
+#define IOTCONNECT_ENV  "your-env"
 
-// prefix for the auto-generated name based on chip HWUID
-#define IOTCONNECT_DUID_PREFIX "e84-va-"
+// If Device Unique ID (DUID) is not provided, a generated DUID will be used using the below prefix
+#define IOTCONNECT_DUID "" 
+// prefix for the auto-generated name based on chip HWUID that will be used if IOTCONNECT_DUID is not supplied
+#define IOTCONNECT_DUID_PREFIX "psoc-edge-va-"
 
 /*
  PEM format certificate and private key
